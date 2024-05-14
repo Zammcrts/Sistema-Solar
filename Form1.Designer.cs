95e92093fba56ee04610f4bb10ab29b709f412a5
Zammcrts <samcortes480@gmail.com>
2024-05-13 23:28:40 -0600

    programa

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Solar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(897, 905);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "▶️";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(969, 905);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "⏸️";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button3.Font = new System.Drawing.Font("CHERRY LIME", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(13, 923);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "CRÉDITOS";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(1847, 914);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(64, 48);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "❌";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(822, 905);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 58);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "⏮️";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(1040, 905);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 58);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "⏭️";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Solar.Properties.Resources.cielo;
+            this.ClientSize = new System.Drawing.Size(1924, 970);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Sistema Solar";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solar
+{
+    public partial class Form1 : Form
+    {
+        // sol 
+        Graphics dibujo;
+        Brush sol = Brushes.Yellow;
+        int solx, soly;
+        //mercurio
+        Brush mercurio = Brushes.PeachPuff;
+        int merx, mery;
+        int mert;
+
+        // venus
+        Brush venus = Brushes.Tomato;
+        int venx, veny;
+        int vent;
+
+        // tierra 
+        Brush tierra = Brushes.ForestGreen;
+        int tiex, tiey;
+        int tiet;
+
+        // marte
+        Brush marte = Brushes.DarkRed;
+        int marx, mary;
+        int mart;
+
+        // jupiter
+        Brush jupiter = Brushes.IndianRed;
+        int jupx, jupy;
+        int jupt;
+
+        // saturno
+        Brush saturno = Brushes.SandyBrown;
+        int satx, saty;
+        int satt;
+
+        // urano
+        Brush urano = Brushes.SteelBlue;
+        int urax, uray;
+        int urat;
+        // neptono
+        Brush nep = Brushes.RoyalBlue;
+        int nepx, nepy;
+        int nept;
+        //luna
+        Brush luna = Brushes.LightGray;
+        int lunax, lunay;
+        int lunat;
+        //anillos
+        Brush ani = Brushes.SandyBrown;
+        int anix, aniy;
+        int anit;
+
+        //cosa rara de los anitos
+        Pen anillo = new Pen(Color.SandyBrown, 10);
+        int anitox, anitoy;
+        int anitot;
+
+        double vel;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.BackColor = Color.Khaki;
+            button6.BackColor = Color.WhiteSmoke;
+            vel = vel -0.5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.BackColor = Color.Khaki;
+            button5.BackColor = Color.WhiteSmoke;
+            vel = vel + 0.5;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            tiempo = tiempo + 0.01*vel;
+            //mercurion
+            merx = solx + (int)(200 * Math.Cos(tiempo * 15));
+            mery = soly + (int)(90 * Math.Sin(tiempo * 15));
+            mert = mery / 16;
+            Refresh();
+
+            //venus
+            venx = solx + (int)(300 * Math.Cos(tiempo * 13));
+            veny = soly + (int)(120 * Math.Sin(tiempo * 13));
+            vent = veny /14;
+
+            //tierra
+            tiex = solx + (int)(400 * Math.Cos(tiempo * 11));
+            tiey = soly + (int)(150 * Math.Sin(tiempo * 11));
+            tiet = tiey / 11;
+
+            //marte
+            marx = solx + (int)(500 * Math.Cos(tiempo * 8));
+            mary = soly + (int)(200 * Math.Sin(tiempo * 8));
+            mart = mary / 8;
+
+            //jupiron
+            jupx = solx + (int)(600 * Math.Cos(tiempo * 6));
+            jupy = soly + (int)(220 * Math.Sin(tiempo * 6));
+            jupt = jupy / 7;
+
+            //luna
+            lunax = tiex + (int)(50 * Math.Cos(tiempo * 30));
+            lunay = tiey + (int)(30 * Math.Sin(tiempo * 30));
+            lunat = lunay / 20;
+
+            //saturon
+            satx = solx + (int)(700 * Math.Cos(tiempo * 4));
+            saty =soly + (int)(230 * Math.Sin(tiempo * 4));
+            satt = saty / 6;
+
+            //anillos saturnro
+            satx = solx + (int)(700 * Math.Cos(tiempo * 4));
+            saty = soly + (int)(230 * Math.Sin(tiempo * 4));
+            satt = saty / 6;
+
+            //urano
+            urax = solx + (int)(760 * Math.Cos(tiempo * 3));
+            uray = soly + (int)(240 * Math.Sin(tiempo * 3));
+            urat = uray / 12;
+
+            // neptuno
+            nepx = solx + (int)(800 * Math.Cos(tiempo * 2));
+            nepy = soly + (int)(250 * Math.Sin(tiempo * 2));
+            nept = nepy / 10;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            solx = 650;
+            soly = 340;
+            merx = 10;
+            mery = 10;
+            venx = 25;
+            veny = 25;
+            tiex = 40;
+            tiey = 40;
+            marx = 35;
+            mary = 35;
+            jupx = 75;
+            jupy = 75;
+            lunax = 5;
+            lunay= 5;
+            satx = 60;
+            saty = 60;
+            nepx = 45;
+            nepx = 45;
+            urax = 50;
+            uray = 50;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor=Color.LightGreen;
+            button2.BackColor = Color.WhiteSmoke;
+            anit = 1;
+            vel = 1;
+            timer1.Start();
+        }
+
+        
+
+        double tiempo;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            dibujo = e.Graphics;
+            dibujo.FillEllipse(sol, solx, soly,150 , 150);
+            //meru
+            dibujo.FillEllipse(mercurio, merx, mery, mert, mert);
+            //venus
+            dibujo.FillEllipse(venus, venx, veny, vent, vent);
+            //tierra
+            dibujo.FillEllipse(tierra, tiex, tiey, tiet, tiet);
+            //marte
+            dibujo.FillEllipse(marte, marx, mary, mart, mart);
+            //jupirt
+            dibujo.FillEllipse(jupiter, jupx, jupy, jupt, jupt);
+            //luna
+            dibujo.FillEllipse(luna, lunax, lunay, lunat, lunat);
+            //saturon
+            dibujo.FillEllipse(saturno, satx, saty, satt, satt);
+            //anillo
+            if (anit == 1)
+            {
+                dibujo.DrawEllipse(anillo, satx - 20, saty + 30, satt + 50, satt - 50);
+            }
+            // urano
+            dibujo.FillEllipse(urano, urax, uray, urat, urat);
+
+            //neptuno
+            dibujo.FillEllipse(nep, nepx, nepy, nept, nept);
+        }
+
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button1.BackColor = Color.WhiteSmoke;
+            button2.BackColor = Color.Tomato;
+            button6.BackColor = Color.WhiteSmoke;
+            button5.BackColor = Color.WhiteSmoke;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+       
+            MessageBox.Show("PROYECTO FINAL: Sistema Solar\n\n" + "AUTOR:Edna Samantha Cortés Carisoza\n\n" + "5 de Diciembre de 2023");
+        }
+    }
+}
